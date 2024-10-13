94b1a341a9d5262157ac0784768c1e79e712536c
natouney <nathan.b2470@gmail.com>
2024-10-13 14:29:47 +0200

    Initial Commit

---
+++ HikeMate/HikeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeMateApp.swift
+//  HikeMate
+//
+//  Created by Nathan on 13/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
